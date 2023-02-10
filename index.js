d2e6dd12fedf778ae427ed3da147a11e7a156e70
Leslie Harden Cooper <lesliehcooper@gmail.com>
2023-02-10 12:11:19 -0600

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: "Leslie",
+    streetAddress: "100 Chicago",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmpl = {...employee};
+    newEmpl[key] = value;
+    return newEmpl;
+
+    const newEmplo = updateEmployeeWithKeyAndValue(employee, "name", "Sam");
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+
+    const destNewEmpl = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+};
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+
+    delete newEmployee.name;
+
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value;
+    return obj;
+
+    delete employee.name;
+
+}
